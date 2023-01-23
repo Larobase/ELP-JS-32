59341376988a6a9e2e10c6a890bfb7d60dc711e3
Maxime Herry <maxime@herry.cc>
2023-01-23 15:57:00 +0100

    essais projet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ELP-JS-32
 Projet javascript pour créer un interpréteur
+utilisateur peut rentrer des lignes de commandes
+cli interprète ces messages
+jouer avec process et await pour exécuter
---
+++ Test.js
@@ -0,0 +1,12 @@
+
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('>>> ', name => {
+  if (name=="lp"){
+    
+  }
+    readline.close();
+  });
